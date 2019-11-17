82797ffc29f43b93d8b191d99944ef7ac3bc91c9
Rust <Rust>
2019-11-17 22:19:12 +0300

    Initial Commit

---
+++ Personal Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Personal Quiz
+//
+//  Created by Рустам Махмудов on 17.11.2019.
+//  Copyright © 2019 Рустам Махмудов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
